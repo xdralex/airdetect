e852309fcf280946117cfe4302532ba78929cf3f
Alexander Demidko <alexander@fluence.one>
2020-01-27 16:09:56 +0300

    Made tensorboard port exposure optional in the docker scripts

--- bin/docker-jupyter.sh
+++ bin/docker-jupyter.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-IMAGE=${1:-xdralex/dl-dev:latest}
-
-JUPYTER_PORT=4004
-TENSORBOARD_PORT=6006
-
+IMAGE=xdralex/dl-dev:latest
 WORK=/home/apollo/package
 
-exec docker run --gpus all --shm-size=8192m --rm -u apollo -w $WORK \
-        -p $JUPYTER_PORT:$JUPYTER_PORT -p $TENSORBOARD_PORT:$TENSORBOARD_PORT \
+if [[ "$1" == "--tensorboard" ]]
+then
+  CONT_TENSORBOARD_PORT=6006
+  HOST_TENSORBOARD_PORT=$2
+  PORT_BINDING="-p $HOST_TENSORBOARD_PORT:$CONT_TENSORBOARD_PORT -p $JUPYTER_PORT:$JUPYTER_PORT"
+  shift; shift
+else
+  PORT_BINDING=""
+fi
+
+exec docker run --gpus all --shm-size=8192m --rm -u apollo -w $WORK $PORT_BINDING \
         -v "$PWD:$WORK" -v "/data/ssd:$WORK/data/ssd" -v "/data/nvme:$WORK/data/nvme" "$IMAGE" \
         jupyter-notebook \
         --NotebookApp.notebook_dir=$WORK \
--- bin/docker-pipeline.sh
+++ bin/docker-pipeline.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-IMAGE=${1:-xdralex/dl-dev:latest}
-
-TENSORBOARD_PORT=6006
-
+IMAGE=xdralex/dl-dev:latest
 WORK=/home/apollo/package
 
-exec docker run --gpus all --shm-size=8192m -i -t --rm -u apollo -w $WORK \
-  -p $TENSORBOARD_PORT:$TENSORBOARD_PORT \
+if [[ "$1" == "--tensorboard" ]]
+then
+  CONT_TENSORBOARD_PORT=6006
+  HOST_TENSORBOARD_PORT=$2
+  PORT_BINDING="-p $HOST_TENSORBOARD_PORT:$CONT_TENSORBOARD_PORT"
+  shift; shift
+else
+  PORT_BINDING=""
+fi
+
+exec docker run --gpus all --shm-size=8192m -i -t --rm -u apollo -w $WORK $PORT_BINDING \
 	-v "$PWD:$WORK" -v "/data/ssd:$WORK/data/ssd" -v "/data/nvme:$WORK/data/nvme" "$IMAGE" \
-	python3.7 pipeline/pipeline.py
+	python3.7 pipeline/pipeline.py "${@:1}"
