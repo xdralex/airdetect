bf39815256c09f9ff06d215b17d98488cc0d5422
Alexander Demidko <alexander@fluence.one>
2020-04-09 15:07:48 +0300

    Added cutmix search space

--- model/airdetect/aircraft_classification/search.py
+++ model/airdetect/aircraft_classification/search.py
@@ -22,4 +22,18 @@ def make_space_dict() -> Dict[str, OrderedDict]:
             ('x_mxp', hp.choice('x_mxp', [0, 1])),
             ('x_mxp_a', hp.loguniform('x_mxp_a', math.log(1e-2), math.log(1)))
         ]),
+
+        'resnet50_cutmix': OrderedDict([
+            ('lrA', hp.uniform('lrA', 1e-4, 4e-4)),
+            ('wdA', hp.loguniform('wdA', math.log(1e-2), math.log(1))),
+            ('lrB', hp.uniform('lrB', 1e-4, 4e-4)),
+            ('wdB', hp.loguniform('wdB', math.log(1e-2), math.log(1))),
+            ('nn_frz', hp.choice('nn_frz', [4])),
+            ('lr_t0', hp.choice('lr_t0', [10])),
+            ('lr_f', hp.choice('lr_f', [2])),
+            ('lr_w', hp.choice('lr_w', [3])),
+            ('x_lbs', hp.loguniform('x_lbs', math.log(1e-4), math.log(1e-1))),
+            ('x_cut', hp.choice('x_cut', [1])),
+            ('x_cut_a', hp.uniform('x_cut_a', 5e-2, 20e-2))
+        ])
     }
